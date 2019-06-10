c165e7f50ed2c1388789eeb1dc6f6c3ba1c27c39
Alex Siqueira de Freitas <asfreitas@desenvvar26-lnx.dimed.local>
2019-06-10 14:06:44 -0300

    Fix for person service

--- src/main/java/com/dimed/java10/FeaturesJava10.java
+++ src/main/java/com/dimed/java10/FeaturesJava10.java
@@ -1,4 +1,28 @@
 package com.dimed.java10;
 
+import com.dimed.shared.Person;
+import com.dimed.shared.PersonService;
+
 public class FeaturesJava10 {
+
+    public void addedVarVariable() {
+
+        var person = new Person(); //infers Person person
+        // it's the same thing
+        // Person person = new Person();
+
+        System.out.println("Var Feature Java 10");
+        System.out.println("Tipo do objeto " + person.getClass().getSimpleName());
+
+    }
+
+    public void optionalElseThrow() {
+        var personOptional = new PersonService().returnPersonList().stream().findFirst();
+
+        // System.out.println(personOptional.get());
+        System.out.println(personOptional.orElseThrow());
+
+
+    }
+
 }
