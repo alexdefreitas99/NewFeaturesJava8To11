7a49f18ffa7c1477efdd0c39176b9ac0156b327a
Alex Siqueira de Freitas <asfreitas@desenvvar26-lnx.dimed.local>
2019-06-10 14:07:18 -0300

    New feature java 11

--- src/main/java/com/dimed/java11/FeaturesJava11.java
+++ src/main/java/com/dimed/java11/FeaturesJava11.java
@@ -1,4 +1,13 @@
 package com.dimed.java11;
 
+import com.dimed.shared.PersonService;
+
 public class FeaturesJava11 {
+    public void newCollectionToArray() {
+        PersonService personService = new PersonService();
+        System.out.println("New Feature Java 11");
+        System.out.println(personService.returnPersonList().toArray()[0]);
+        System.out.println(personService.returnPersonList());
+
+    }
 }
