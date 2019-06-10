a485a07bded14da3a2b81e214abea3f30c14f9cb
Alex Siqueira de Freitas <asfreitas@desenvvar26-lnx.dimed.local>
2019-06-10 14:07:05 -0300

    Fix for person service

--- src/main/java/com/dimed/java8/FeaturesJava8.java
+++ src/main/java/com/dimed/java8/FeaturesJava8.java
@@ -1,34 +1,17 @@
 package com.dimed.java8;
 
 import com.dimed.initializer.MenuOpcoes;
+import com.dimed.shared.Person;
+import com.dimed.shared.PersonService;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class FeaturesJava8 {
 
     MenuOpcoes menuOpcoes = new MenuOpcoes();
+    PersonService personService = new PersonService();
 
-    public List<Person> returnPersonList(){
-        List<Person> personList = new ArrayList<>();
-
-        Person person = new Person();
-        person.name = "alex";
-        person.gender = Person.Sex.MALE;
-        person.emailAddress = "alexdefreitas99@hotmail.com";
-
-        personList.add(person);
-
-        person = new Person();
-        person.name = "Stive";
-        person.gender = Person.Sex.MALE;
-        person.emailAddress = "stive@hotmail.com";
-
-        personList.add(person);
-
-        return personList;
-    }
 
     public void featureLambda() {
 //        returnPersonList().forEach(eachPerson -> System.out.println(eachPerson));
@@ -36,11 +19,10 @@ public class FeaturesJava8 {
         System.out.println("#####################");
         System.out.println("Lambda new feature implemented in java 8");
 
-
-        returnPersonList().stream().filter(
+        personService.returnPersonList().stream().filter(
                 p -> p.getGender() == Person.Sex.MALE
-                && p.getName() == "alex"
-        ).map( p -> p.getEmailAddress())
+                        && p.getName() == "alex"
+        ).map(p -> p.getEmailAddress())
                 .forEach(email -> System.out.println(email));
 
         System.out.println("#####################");
@@ -48,7 +30,7 @@ public class FeaturesJava8 {
 
     }
 
-    public void featureImprovedTypeReference(){
+    public void featureImprovedTypeReference() {
         // in java < 8
         // listPerson.addAll(Arrays.<Person>asList());
         System.out.println("#####################");
@@ -56,7 +38,7 @@ public class FeaturesJava8 {
 
         System.out.println("Type Improvement Java 8");
 
-        List <Person>  listPerson = returnPersonList();
+        List<Person> listPerson = personService.returnPersonList();
         listPerson.addAll(Arrays.asList());
         System.out.println(listPerson);
         System.out.println("#####################");
